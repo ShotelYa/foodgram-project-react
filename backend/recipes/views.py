832c2295e85645f825e6ab445eef60f44f25b02b
ShotelYa <shotel020294@gmail.com>
2022-11-01 12:50:41 +0200

    v9

--- backend/recipes/views.py
+++ backend/recipes/views.py
@@ -1,7 +1,7 @@
 from django.db.models import Sum
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
-from pagination import CustomPagination
+from .pagination import CustomPagination
 from recipes.serializers import (CreateRecipeSerializer, IngredientSerializer,
                                  RecipeSerializerShort, TagSerializer)
 from requests import Response

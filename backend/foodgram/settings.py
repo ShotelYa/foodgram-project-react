38a3b99792053fff6057446b985b30fce4af31e7
ShotelYa <shotel020294@gmail.com>
2022-11-05 15:45:27 +0200

    v10

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -124,7 +124,7 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
     ],
     'DEFAULT_PAGINATION_CLASS':
-    'rest_framework.pagination.PageNumberPagination',
+    'recipes.panination.CustomPagination',
     'PAGE_SIZE':
     6,
 }

4c9c079da89ab94af0ae3e5d37c63d691cf543dc
ShotelYa <shotel020294@gmail.com>
2022-11-23 18:26:12 +0200

    1.3

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -79,7 +79,13 @@ class FollowSerializer(serializers.ModelSerializer):
         ]
 
     def get_is_subscribed(self, obj):
-        return Follow.objects.filter(user=obj.user, author=obj.id).exists()
+        user = self.context.get("request").user
+        if user.is_anonymous:
+            return False
+        return Follow.objects.filter(user=user, following=obj.id).exists()
+
+    # def get_is_subscribed(self, obj):
+    #     return Follow.objects.filter(user=obj.user, author=obj.id).exists()
 
     def get_recipes(self, obj):
         recipes = Recipe.objects.filter(author=obj.author)

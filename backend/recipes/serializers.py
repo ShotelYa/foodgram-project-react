54cd86ac19f388e7e6f97338f8ab6ddcc8d929be
ShotelYa <shotel020294@gmail.com>
2022-11-23 18:54:40 +0200

    1.3

--- backend/recipes/serializers.py
+++ backend/recipes/serializers.py
@@ -93,7 +93,7 @@ class ListRecipeSerializer(serializers.ModelSerializer):
         user = self.context.get('request').user
         if user.is_anonymous:
             return False
-        return Favorite.objects.filter(recipe=obj, user=user).exists()
+        return Recipe.objects.filter(favorites__user=user, id=obj.id).exists()
 
     # def get_is_favorited(self, obj):
     #     request = self.context.get('request')

934a4eeee665ef25d0d7b35d35bba4f7324e7bed
ShotelYa <shotel020294@gmail.com>
2022-11-26 14:35:26 +0200

    1.4

--- backend/recipes/serializers.py
+++ backend/recipes/serializers.py
@@ -1,10 +1,10 @@
 from django.contrib.auth import get_user_model
-from django.forms import ValidationError
+# from django.forms import ValidationError
 from drf_extra_fields.fields import Base64ImageField
 from rest_framework import serializers
 from users.serializers import CustomUserSerializer
 
-from .models import Cart, Favorite, Ingredient, IngredientRecipe, Recipe, Tag
+from .models import Favorite, Ingredient, IngredientRecipe, Recipe, Tag
 
 User = get_user_model()
 

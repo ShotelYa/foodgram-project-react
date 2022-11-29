3104bbd9a075a9ab9e72b211eb9108d4fe003531
ShotelYa <shotel020294@gmail.com>
2022-11-29 18:32:18 +0200

    v2

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -4,7 +4,7 @@ from users.models import Follow
 
 @admin.register(Follow)
 class FollowAdmin(admin.ModelAdmin):
-    list_display = ('user', 'following')
-    search_fields = ('user', 'following')
+    list_display = ('user', 'author')
+    search_fields = ('user', 'author')
     list_filter = ('id', 'user')
     empty_value_display = '-NONE-'

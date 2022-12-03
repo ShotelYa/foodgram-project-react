1a39a1e85071d66d2d5d969d6c7fa3356770a4ee
ShotelYa <shotel020294@gmail.com>
2022-12-03 17:48:06 +0200

    v2.2

--- backend/recipes/serializers.py
+++ backend/recipes/serializers.py
@@ -47,7 +47,7 @@ class ListRecipeSerializer(serializers.ModelSerializer):
     tags = TagSerializer(many=True, read_only=True)
     author = CustomUserSerializer(read_only=True)
     ingredients = serializers.SerializerMethodField(
-        source='ingredientrecipe_set',
+        source='ingredientamount_set',
         many=True,
         read_only=True,
     )
